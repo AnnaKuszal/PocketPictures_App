313795b5c2dc56177feb1a1f38655796f6e55ea8
Anna Kuszal <anna.kuszal@o2.pl>
2022-03-14 12:10:38 +0100

    Add 'MainLayout’ (component & styles)

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainLayout.module.scss';
+
+
+const MainLayout = ({ children }) => (
+  <div className={styles.root}>
+    {children}
+  </div>
+);
+
+
+export default MainLayout;
